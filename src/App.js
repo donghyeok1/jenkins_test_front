93dc3e0cc648e4fc5302daac3cb170ec88b13cc3
donghyeok1 <07385@naver.com>
2023-05-02 12:44:00 +0900

    dasfa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-const backEnvVar = process.env.BACKEND_SVC_NAME;
-
 function App() {
   const [datas, setDatas] = React.useState([]);
   React.useEffect(() => {
-    fetch(`http://${backEnvVar}:8000/dev/data`, {
+    fetch("http://192.168.133.60:8000/dev/data/", {
       method: "GET",
     })
       .then(res => {
